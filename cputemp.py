6fe247df602fb788c1c2264d922ff637707bf479
Paul Osborne <osbpau@gmail.com>
2012-11-17 11:07:08 -0600

    Add linux support for all stats

--- cputemp.py
+++ cputemp.py
@@ -1,29 +1,66 @@
 from argparse import ArgumentParser
 import bottle
 import json
-import wmi
+import sys
 
-wmi_computer = wmi.WMI()
-ohm = wmi.WMI(namespace='root\\OpenHardwareMonitor')
+# globals
+g_sysinfo_provider = None
 
-#===============================================================================
-# API Helpsers
-#===============================================================================
-def get_cpu_load():
-    """Return the current average CPU load across all processor cores"""
-    wql = "SELECT * FROM Win32_PerfFormattedData_Counters_ProcessorInformation WHERE NAME = '_Total'"
-    res = wmi_computer.query(wql)
-    return 100 - int(res[0].PercentIdleTime)
 
-def get_free_memory():
-    return int(wmi_computer.Win32_OperatingSystem()[0].FreePhysicalMemory)
+class WindowsSystemInformationProvider(object):
 
-def get_total_memory():
-    return int(wmi_computer.Win32_ComputerSystem()[0].TotalPhysicalMemory)
+    def __init__(self):
+        import wmi
+        self.wmi = wmi.WMI()
+        self.ohm = wmi.WMI(namespace='root\\OpenHardwareMonitor')
 
-def get_cpu_temperature():
-    """Get the CPU temperature in Celsius"""
-    return ohm.query("select * from Sensor where Identifier = '/amdcpu/0/temperature/0'")[0].Value; 
+    def get_cpu_load(self):
+        """Return the current average CPU load across all processor cores"""
+        wql = "SELECT * FROM Win32_PerfFormattedData_Counters_ProcessorInformation WHERE NAME = '_Total'"
+        res = self.wmi.query(wql)
+        return 100 - int(res[0].PercentIdleTime)
+
+    def get_free_memory(self):
+        return int(self.wmi.Win32_OperatingSystem(self)[0].FreePhysicalMemory)
+
+    def get_total_memory(self):
+        return int(self.wmi.Win32_ComputerSystem(self)[0].TotalPhysicalMemory)
+
+    def get_cpu_temperature(self):
+        """Get the CPU temperature in Celsius"""
+        return self.ohm.query("select * from Sensor where Identifier = '/amdcpu/0/temperature/0'")[0].Value; 
+
+
+class LinuxSystemInformationProvider(object):
+
+    def __init__(self):
+        import psutil
+        self.psutil = psutil
+
+    def _get_temp(self):
+        import sensors
+        sensors.init()
+        for chip in sensors.iter_detected_chips():
+            if not 'temp' in str(chip):
+                continue
+            tot = 0
+            for i, feature in enumerate(chip):
+                tot += feature.get_value()
+            return float(tot) / (i + 1)
+        return None
+
+    def get_cpu_load(self):
+        return self.psutil.cpu_percent(interval=1)
+
+    def get_free_memory(self):
+        return self.psutil.virtual_memory().free
+
+    def get_total_memory(self):
+        return self.psutil.virtual_memory().total
+
+    def get_cpu_temperature(self):
+        return self._get_temp()
+
 
 #===============================================================================
 # API Views
@@ -37,22 +74,22 @@ def index():
 def stats_dump():
     bottle.response.content_type = "application/json"
     return json.dumps({
-        "cpu_load_percent": get_cpu_load(),
-        "total_memory": get_total_memory(),
-        "free_memory": get_free_memory(),
-        "cpu_temp_celsius": get_cpu_temperature(),
+        "cpu_load_percent": g_sysinfo_provider.get_cpu_load(),
+        "total_memory": g_sysinfo_provider.get_total_memory(),
+        "free_memory": g_sysinfo_provider.get_free_memory(),
+        "cpu_temp_celsius": g_sysinfo_provider.get_cpu_temperature(),
     })
 
 
 @bottle.route("/api/temperature")
 def get_temperature():
-    return json.dumps({"cpu_temp_celsius": get_cpu_temperature()})
+    return json.dumps({"cpu_temp_celsius": g_sysinfo_provider.get_cpu_temperature()})
 
 
 @bottle.route("/api/cpuload")
 def cpu_load():
     bottle.response.content_type = "application/json"
-    return json.dumps({"cpu_load_percent": get_cpu_load(), })
+    return json.dumps({"cpu_load_percent": g_sysinfo_provider.get_cpu_load(), })
 
 #===============================================================================
 # Plumbing
@@ -65,7 +102,12 @@ def parse_arguments():
     return parser.parse_args()
 
 def main():
+    global g_sysinfo_provider
     args = parse_arguments()
+    if sys.platform.startswith('win'):
+        g_sysinfo_provider = WindowsSystemInformationProvider()
+    else:
+        g_sysinfo_provider = LinuxSystemInformationProvider()
     bottle.run(host='0.0.0.0', port=args.port)
 
 if __name__ == '__main__':
